000c9ac1a14e37a8828f6522576e25afb7052d65
Åsa Erlfeldt <asaerlfeldt@gmail.com>
2021-05-30 22:32:33 +0200

    Add files via upload

---
+++ js_fade.js
@@ -0,0 +1,7 @@
+/* var text = "We Are a Design Studio From Columbus, Ohio";
+
+console.log(text.length);
+
+for (var i = 0; i < text.length; i++){
+  $('.container').append('<span>' + text.length[i] + '</span>');
+} */
